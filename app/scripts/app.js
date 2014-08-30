4b7b7f9d91fee7ee4370cb15beccc9908fa148c4
Jason Stathopulos <jason@adluremedia.com>
2014-08-30 19:50:07 -0400

    Remove Default Pages
    Remove the default pages from the project because those pages are not
    being used for this site

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,16 +15,4 @@ angular
     'ngRoute'
   ])
   .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
   });
