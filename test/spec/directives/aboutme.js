29605e3b157c71a965991f91e39a686ba2fc6422
Jason Stathopulos <jason@adluremedia.com>
2014-09-03 17:23:03 -0400

    Fix When Directive Unit Test Will Fail
    Fix an error when the directive unit test will fail because those were
    the default test when the directives were created however those have
    change so those test not need now.

--- test/spec/directives/aboutme.js
+++ test/spec/directives/aboutme.js
@@ -5,16 +5,16 @@ describe('Directive: aboutMe', function () {
   // load the directive's module
   beforeEach(module('spiritBreaker226GitHubPageApp'));
 
-  var element,
-    scope;
+  // var element,
+  //   scope;
 
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
-  }));
+  // beforeEach(inject(function ($rootScope) {
+  //   scope = $rootScope.$new();
+  // }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<about-me></about-me>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the aboutMe directive');
-  }));
+  // it('should make hidden element visible', inject(function ($compile) {
+  //   element = angular.element('<about-me></about-me>');
+  //   element = $compile(element)(scope);
+  //   expect(element.text()).toBe('this is the aboutMe directive');
+  // }));
 });
--- test/spec/directives/carouselFeature.js
+++ test/spec/directives/carouselFeature.js
@@ -5,16 +5,16 @@ describe('Directive: carouselFeature', function () {
   // load the directive's module
   beforeEach(module('spiritBreaker226GitHubPageApp'));
 
-  var element,
-    scope;
+  // var element,
+  //   scope;
 
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
-  }));
+  // beforeEach(inject(function ($rootScope) {
+  //   scope = $rootScope.$new();
+  // }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<carousel-feature></carousel-feature>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the carouselFeature directive');
-  }));
+  // it('should make hidden element visible', inject(function ($compile) {
+  //   element = angular.element('<carousel-feature></carousel-feature>');
+  //   element = $compile(element)(scope);
+  //   expect(element.text()).toBe('this is the carouselFeature directive');
+  // }));
 });
