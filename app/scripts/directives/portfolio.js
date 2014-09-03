9ece6d4ad55fc3985ba918c95df74790fe59c248
Jason Stathopulos <jason@adluremedia.com>
2014-09-03 17:20:29 -0400

    Add Projects From Project JSON File
    Add the projects from project JSON file to the Portfolio section
    because those will display the best projects that I have done as of now.

--- app/scripts/directives/portfolio.js
+++ app/scripts/directives/portfolio.js
@@ -7,9 +7,38 @@
  * # portfolio
  */
 angular.module('spiritBreaker226GitHubPageApp')
+  .controller('PortfolioCtrl', function($scope, $http){
+  	var portfolio = this;
+
+  	portfolio.projects = [];
+
+  	// checks if index is odd after 1
+	// because the clearfix can only be used in the middle of a row
+	this.showClearfix = function (indexOfProject) {
+		return indexOfProject !== 3 && (indexOfProject % 2) === 1;
+	};// end of showClearfix()
+
+	// checks if there is an end date for the project
+	this.hideProjectEndDate = function (dateProjectEndDate) {
+		return dateProjectEndDate === '';
+	};// end of hideProjectEndDate())
+
+	// calls the file 
+	$http.get('scripts/siteContentProjects.json').success(function(data) {
+		// loads the data into an service for data 
+		// if the http gets is able to access the data
+		portfolio.projects = data;
+		portfolio.numberOfProjects = (Math.ceil(portfolio.projects.length / 4) + 1);
+
+		// this scope service is for the select element in order for it to do a selction
+		// when the page first loads up
+		//portfolio.projectOrder = 'name';
+	});
+  })
   .directive('portfolio', function () {
     return {
       templateUrl: 'views/templates/portfolio.html',
-      restrict: 'E'
+      restrict: 'E',
+      controllerAs: 'project'
     };
   });
