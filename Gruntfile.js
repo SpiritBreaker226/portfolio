a56ebbe1c883ef891a57a7a9210f5bfcc472f50d
Jason Stathopulos <jason@adluremedia.com>
2014-09-06 19:02:18 -0400

    Prevents Renaming Fonts & Images On Live Build
    Prevents renaming of fonts and images on the live builds because grunt
    does not rename the image tags to the new of the images nor updates the
    CSS for with the new name of the fonts.

--- Gruntfile.js
+++ Gruntfile.js
@@ -160,9 +160,7 @@ module.exports = function (grunt) {
       dist: {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
-          '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= yeoman.dist %>/styles/fonts/*'
+          '<%= yeoman.dist %>/styles/{,*/}*.css'
         ]
       }
     },
