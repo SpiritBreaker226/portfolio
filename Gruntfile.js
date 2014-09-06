147d7a942490aa628dedfceb4f3562041f6a7bc2
Jason Stathopulos <jason@adluremedia.com>
2014-09-06 19:50:54 -0400

    Fix Font-Awesome Fonts Would Not Copy Over
    Fix Font-Awesome fonts would not copy over during live build because
    grunt was not configure to add in those fonts to the live build

--- Gruntfile.js
+++ Gruntfile.js
@@ -307,6 +307,11 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>/bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '<%= yeoman.app %>/bower_components/font-awesome',
+          src: 'fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
