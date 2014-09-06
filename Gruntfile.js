18b281be2b3009d962f278153201b57ca276f8ac
Jason Stathopulos <jason@adluremedia.com>
2014-09-06 19:30:13 -0400

    Fix JSON Files Would Not Copy Over To Script
    Fix JSON files would not copy over to script folder during live
    build because grunt was not configure copy over JSON files

--- Gruntfile.js
+++ Gruntfile.js
@@ -294,7 +294,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/*'
+            'styles/fonts/*',
+			'scripts/{,*/}*.json'
           ]
         }, {
           expand: true,
