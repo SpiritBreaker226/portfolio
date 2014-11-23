b6fa9ca381d0a5d15a536ef444f7a4ca3f20b0a5
Jason Stathopulos <jason@adluremedia.com>
2014-11-23 16:18:11 -0500

    Fix Positioning Of View Buttons in Project Detail
    Fix positioning of view buttons in the project details, on smaller size
    screens, since they where in the middle of the screen. Because of this a
    directive was, for the buttons, in order to have them in two areas. So
    that when on a smaller size screens, the view buttons, in the project
    description, will be hidden. Then the view buttons, in the sidebar, will
    then be displayed. This will allow the buttons to be at the bottom of
    the section always.

---
+++ app/scripts/directives/viewbuttons.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name spiritBreaker226GitHubPageApp.directive:viewButtons
+ * @description
+ * # viewButtons
+ */
+angular.module('spiritBreaker226GitHubPageApp')
+  .directive('viewButtons', function () {
+    return {
+      templateUrl: 'views/templates/viewbuttons.html',
+      restrict: 'E'
+    };
+  });
