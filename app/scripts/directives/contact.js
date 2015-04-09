a2b58c910342922ebe634dfa92983a724d72297a
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-09 17:44:56 -0400

    Add Custom Thank You Message For Contact Form

--- app/scripts/directives/contact.js
+++ app/scripts/directives/contact.js
@@ -1,12 +1,22 @@
 'use strict';
 
+var app = angular.module('spiritBreaker226GitHubPageApp');
+
 /**
  * @ngdoc directive
  * @name spiritBreaker226GitHubPageApp.directive:contact
  * @description
  * # contact
  */
-angular.module('spiritBreaker226GitHubPageApp')
+app
+	.controller('ContactCtrl', function($scope){
+		$scope.contactThankYouMesage = function() {
+			angular.element('#contact-thank-you-message').html('<p>Thank you, your question has been submitted</p>');
+			angular.element('#contact-form-container').fadeOut({
+				duration: 500
+			});
+		};
+	})  
   .directive('contact', function () {
     return {
       templateUrl: 'views/templates/contact.html',
