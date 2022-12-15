023c3a9c850d860e125b6ab882fc30d8670d8878
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-15 10:21:17 -0800

    Add App Header Component
    Add an App Header component so that the application name and the
    navigation are displayed to the user constantly.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import ErrorBoundary from './Components/ErrorBoundary'
 import { ThemeProvider } from './theme'
 import { AppFooter } from './AppFooter'
+import { AppHeader } from './AppHeader'
 
 const App = () => (
   <ErrorBoundary>
     <ThemeProvider>
       <Router>
+        <AppHeader />
+
         <AppFooter />
       </Router>
     </ThemeProvider>
