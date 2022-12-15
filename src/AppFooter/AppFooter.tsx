e936d5980c153ef8e3700569e3552dfbd88600ca
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-14 20:30:55 -0800

    Add App Footer Component
    Add the App Footer component so there is a place where all the social
    media links are located, and it is easy to contact the developer.

---
+++ src/AppFooter/AppFooter.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react'
+import styled from 'styled-components'
+
+import { SocialMediaLink } from './SocialMediaLink'
+
+const Container = styled.footer`
+  padding: 3rem;
+  background-color: ${(props) => props.theme.primary};
+`
+
+const Title = styled.h3`
+  color: ${(props) => props.theme.secondary};
+  margin-bottom: 2rem;
+`
+
+const SocialMediaContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 3rem;
+`
+
+export const AppFooter: FC = () => (
+  <Container>
+    <Title>Find Me On Social Media</Title>
+
+    <SocialMediaContainer>
+      <SocialMediaLink type="twitter" link="https://twitter.com/JasonStats" />
+      <SocialMediaLink type="dev" link="https://dev.to/jstats" />
+      <SocialMediaLink
+        type="linkedin"
+        link="https://www.linkedin.com/in/jasonstathopulos"
+      />
+      <SocialMediaLink
+        type="github"
+        link="https://github.com/SpiritBreaker226"
+      />
+      <SocialMediaLink
+        type="instagram"
+        link="http://instagram.com/jasonstats"
+      />
+    </SocialMediaContainer>
+  </Container>
+)
---
+++ src/AppFooter/SocialMediaLink.tsx
@@ -0,0 +1,48 @@
+import {
+  findIconDefinition,
+  IconDefinition,
+  IconLookup,
+  IconName,
+  library,
+} from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FC } from 'react'
+import styled from 'styled-components'
+
+const Link = styled.a`
+  border-radius: 8px;
+  padding: 0.5rem 1rem;
+  background-color: ${(props) => props.theme.secondary};
+
+  &:active,
+  &:hover {
+    background-color: ${(props) => props.theme.primary};
+    color: ${(props) => props.theme.secondary};
+  }
+`
+
+// needs to be here so that it can load the fontawesome icons
+library.add(fab)
+
+export type SocialMediaLinkProps = {
+  link: string
+  type: IconName
+}
+
+export const SocialMediaLink: FC<SocialMediaLinkProps> = ({ type, link }) => {
+  const iconLookup: IconLookup = { prefix: 'fab', iconName: type }
+  const iconDefinition: IconDefinition = findIconDefinition(iconLookup)
+
+  return (
+    <Link
+      href={link}
+      title={`See Jason Stathopulos account on ${type[0].toUpperCase()}${type
+        .slice(1)
+        .toLowerCase()}`}
+      target="_blank"
+      rel="noreferrer">
+      <FontAwesomeIcon icon={iconDefinition} size="4x" />
+    </Link>
+  )
+}
