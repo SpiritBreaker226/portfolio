f6cd05baf7154bfad22847add1ad9c3e4a053f19
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-17 16:36:45 -0500

    Update Skillset to Use Project Tags
    Update the Skillset to use the Project's Tags so that the skillsets are
    in-sync to show the skills the developer has and which project they
    belong to.

--- src/data/skillSet.ts
+++ src/data/skillSet.ts
@@ -1,14 +1,22 @@
-export const skillSet = {
+import { Tag } from '../types'
+
+type SkillSet = { [k in Tag]: number }
+
+export const skillSet: SkillSet = {
   'Ruby on Rails': 6.5,
   Ruby: 5.5,
   'C++': 5,
+  'C#': 3,
+  'Objective-C': 3,
+  PHP: 4,
   JavaScript: 8.5,
   ReactJS: 8,
-  Redux: 7.5,
-  JSON: 7.5,
-  HTML5: 7.5,
-  CSS3: 6,
-  MUI: 7.5,
+  Phonegap: 4,
+  HTML: 7.5,
+  CSS: 6,
   SQL: 7,
+  'SQL Server': 7,
+  PostgreSQL: 6.5,
+  mySQL: 5.5,
   Git: 9,
 }
