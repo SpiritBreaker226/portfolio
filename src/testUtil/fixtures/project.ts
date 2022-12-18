ff195fac6be605158c44fa663b065ef4bb0fe23e
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-18 17:26:56 -0500

    Add Testing Fixtures For Project
    Add the testing fixtures for Project so that we have test data to use
    for testing.

---
+++ src/testUtil/fixtures/project.ts
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker'
+
+import { Platform, Project, ProjectType, Tag } from '../../types'
+
+export const project: Project = {
+  id: faker.datatype.uuid(),
+  name: faker.company.name(),
+  summary: faker.hacker.phrase(),
+  responsibilities: faker.helpers.arrayElements([
+    faker.lorem.sentence(7),
+    faker.lorem.sentence(6),
+    faker.lorem.sentence(5),
+  ]),
+  tags: faker.helpers.arrayElements([
+    Tag['C#'],
+    Tag['JavaScript'],
+    Tag['CSS'],
+    Tag['HTML'],
+    Tag['SQLServer'],
+    Tag['Phonegap'],
+  ]),
+  teamSize: Number(faker.random.numeric()),
+  platforms: faker.helpers.arrayElements([
+    Platform['mobile'],
+    Platform['web'],
+    Platform['game'],
+  ]),
+  type: faker.helpers.arrayElement([ProjectType['Close'], ProjectType['Open']]),
+  url: faker.helpers.arrayElement([faker.internet.url(), '']),
+  sampleCodeUrl: faker.helpers.arrayElement([faker.internet.url(), '']),
+  isFeature: faker.datatype.boolean(),
+}
