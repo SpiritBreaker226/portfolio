ac47f5380bbb03322a0292fe6bc9eec4e207d78a
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-18 15:37:21 -0500

    Add `useInfiniteScroll` to Load Batch of Items
    Add the `useInfiniteScroll` to load a batch of Items at a time, so there
    is no large scroll when the page first loads. Reducing the application's
    performance as it needs to load all of the items at once.

---
+++ src/hooks/useInfiniteScroll.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+import { Waypoint } from '../Components'
+
+type InfiniteScrollReturn<T> = {
+  fetchMore: (pointToFetchMore: number) => JSX.Element | null
+  items: T[]
+}
+
+/**
+ * hook to do infinite scrolling without having the logic in the code directly
+ * @param {T[]} items - Generalize type of the array to use
+ * @param {number} limit - number of items to use per page
+ * @param {number} defaultPage - which page to start the search
+ *
+ * @returns {object}
+ */
+function useInfiniteScroll<T>(
+  items: T[],
+  limit = 24,
+  defaultPage = 1
+): InfiniteScrollReturn<T> {
+  const [page, setPage] = useState(defaultPage)
+  const numberOfUsers = limit * page + 1
+
+  const fetchMore = (pointToFetchMore: number) =>
+    pointToFetchMore + 10 === limit * page ? (
+      <Waypoint onEnter={() => setPage(page + 1)} />
+    ) : null
+
+  return {
+    fetchMore,
+    items: items.slice(0, numberOfUsers),
+  }
+}
+
+export default useInfiniteScroll
