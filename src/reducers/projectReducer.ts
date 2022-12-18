0635aa6b544e4e32437eb892f27eafcae6e1e346
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-18 16:27:16 -0500

    Add Custom Redux State Management
    Add a custom Redux state management as Redux is too much for this type
    of application, where it does need state management to handle a global
    state.

---
+++ src/reducers/projectReducer.ts
@@ -0,0 +1,25 @@
+import { Action, InitialState, Projects, Types } from '../types'
+
+export const projectReducer = (state: InitialState, action: Action) => {
+  switch (action.type) {
+    case Types.AddProject:
+      const projects = action.payload.projects.reduce(
+        (prevProject: Projects, project) => {
+          prevProject[project.id] = project
+
+          return prevProject
+        },
+        {}
+      )
+
+      return {
+        ...state,
+        projects,
+      }
+    case Types.SelectedProject:
+      return {
+        ...state,
+        selectedProject: action.payload.selectedProject,
+      }
+  }
+}
--- src/testUtil/index.ts
+++ src/testUtil/index.ts
@@ -1,2 +1,3 @@
+export * from './AppProvider'
 export * from './DefaultWrapper'
 export * from './render'
