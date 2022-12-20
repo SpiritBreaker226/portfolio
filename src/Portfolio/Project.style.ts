93d658439e560f7480ba324101faa324dda2eb3e
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-20 18:21:40 -0500

    Move Project Icon Style to Project Style File
    Move the Project Icon style to the Project Style file, as there are
    multiple places in the Portfolio section where the icon styling is used
    on the icons of the projects.

---
+++ src/Portfolio/Project.style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Icon = styled.img`
+  border-radius: 50%;
+  border: 1px solid ${(props) => props.theme.colors.border};
+  height: 128px;
+  width: 128px;
+  margin-bottom 1rem;
+`
