c2cc18dafb63587f354a2b057301be415a5b1b32
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-20 11:10:57 -0500

    Rename Summary to Description in Project Data
    Rename the Summary to Description element in Project Data as that better
    describes what the element is doing.

--- src/testUtil/fixtures/project.ts
+++ src/testUtil/fixtures/project.ts
@@ -5,7 +5,7 @@ import { Platform, Project, ProjectType, Tag } from '../../types'
 export const project: Project = {
   id: faker.datatype.uuid(),
   name: faker.company.name(),
-  summary: faker.hacker.phrase(),
+  description: faker.hacker.phrase(),
   responsibilities: faker.helpers.arrayElements([
     faker.lorem.sentence(7),
     faker.lorem.sentence(6),
@@ -27,6 +27,7 @@ export const project: Project = {
   ]),
   type: faker.helpers.arrayElement([ProjectType['Close'], ProjectType['Open']]),
   url: faker.helpers.arrayElement([faker.internet.url(), '']),
+  icon: faker.image.business(128, 128),
   sampleCodeUrl: faker.helpers.arrayElement([faker.internet.url(), '']),
   isFeature: faker.datatype.boolean(),
 }
