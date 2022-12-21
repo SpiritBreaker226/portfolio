d80657823cb273ac1f58274f6bb1e5952788dcc4
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-21 12:07:41 -0500

    Update Portfolio to Use App State Project
    Update the Portfolio section to use the application state project value
    so that the application uses that and limits the use of the project in
    the data folder. The application should treat the project in the data
    folder as if it is from a server. Thus, it makes it easier to move to a
    server if needed. Plus, it helps the application's maintainability as
    the number of references to the data folder reduces.

--- src/Portfolio/Portfolio.tsx
+++ src/Portfolio/Portfolio.tsx
@@ -1,14 +1,29 @@
-import { FC } from 'react'
+import { FC, useCallback, useEffect } from 'react'
 
+import { useApp } from '../context'
 import { usePortfolio } from '../hooks'
+import { Types } from '../types'
 import { ProjectList } from './ProjectList'
 
 export const Portfolio: FC = () => {
+  const {
+    state: { projects },
+    dispatch,
+  } = useApp()
   const { getPortfolios } = usePortfolio()
+  const dispatchProjects = useCallback(getPortfolios, [getPortfolios])
+  const dataProjects = dispatchProjects()
+
+  useEffect(() => {
+    dispatch({
+      type: Types.AddProject,
+      payload: { projects: dataProjects },
+    })
+  }, [dispatch, dataProjects])
 
   return (
     <section>
-      <ProjectList projects={getPortfolios()} />
+      <ProjectList projects={Object.values(projects)} />
     </section>
   )
 }
