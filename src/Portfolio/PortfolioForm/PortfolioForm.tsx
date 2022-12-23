635f64eaf3b35c3aeee0a8409c80dc7b956342fd
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-22 19:03:14 -0500

    Add Non Display Search to Portfolio Form
    Add the non-display search to the Portfolio Form, as those are the
    primary ways the user would do all their searches.

--- src/Portfolio/PortfolioForm/PortfolioForm.tsx
+++ src/Portfolio/PortfolioForm/PortfolioForm.tsx
@@ -1,15 +1,18 @@
 import { ChangeEvent, FC } from 'react'
 import styled from 'styled-components'
 import { titleCase } from '../../AppBody/helpers'
+import { Button, Textfield } from '../../Components'
 
 import { useApp } from '../../context'
 import {
   DisplayOption,
   displayOptions,
+  Platform,
+  ProjectType,
   Types,
   UpdateSearchTypes,
 } from '../../types'
-import { RadioButton } from './Components'
+import { Checkbox, RadioButton } from './Components'
 
 const Container = styled.div`
   padding-bottom: 1rem;
@@ -17,6 +20,19 @@ const Container = styled.div`
 
 const FieldsetLabel = styled.h4`
   padding-bottom: 0.25rem;
+`
+
+const NonDisplayFilterOptions = styled.fieldset`
+  display: flex;
+`
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+`
+
+const ResetSearchContainer = styled.div`
+  text-align: right;
 `
 
 export const PortfolioForm: FC = () => {
@@ -29,6 +45,68 @@ export const PortfolioForm: FC = () => {
     dispatch({
       type: UpdateSearchTypes.Display,
       payload: { display: e.target.value as DisplayOption },
+    })
+
+    dispatch({
+      type: Types.Search,
+      payload: {},
+    })
+  }
+
+  const handleSearchTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: UpdateSearchTypes.Text,
+      payload: { searchText: e.target.value },
+    })
+
+    dispatch({
+      type: Types.Search,
+      payload: {},
+    })
+  }
+
+  const handleTypeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const type =
+      e.target.value === 'all' ? undefined : (e.target.value as ProjectType)
+
+    dispatch({
+      type: UpdateSearchTypes.Type,
+      payload: { type },
+    })
+
+    dispatch({
+      type: Types.Search,
+      payload: {},
+    })
+  }
+
+  const handlePlatformChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const platform = e.target.value as Platform
+    const newSearchPlatforms = new Set(searchCriteria.platforms)
+
+    if (newSearchPlatforms.has(platform)) {
+      newSearchPlatforms.delete(platform)
+    } else {
+      newSearchPlatforms.add(platform)
+    }
+
+    dispatch({
+      type: UpdateSearchTypes.Platform,
+      payload: {
+        platforms: newSearchPlatforms,
+      },
+    })
+
+    dispatch({
+      type: Types.Search,
+      payload: {},
+    })
+  }
+
+  const handleResetSearch = () => {
+    dispatch({
+      type: Types.ResetSearchCriteria,
+      payload: {},
     })
 
     dispatch({
@@ -53,6 +131,58 @@ export const PortfolioForm: FC = () => {
           />
         ))}
       </fieldset>
+      <NonDisplayFilterOptions>
+        <SearchContainer>
+          <label htmlFor="searchText">Name / Description</label>
+
+          <Textfield
+            type="text"
+            id="searchText"
+            onChange={handleSearchTextChange}
+            value={searchCriteria.searchText}
+          />
+        </SearchContainer>
+        <SearchContainer>
+          <FieldsetLabel>Types</FieldsetLabel>
+          <RadioButton
+            label="All"
+            id="all"
+            value="all"
+            name="type"
+            onChange={handleTypeChange}
+            checked={!searchCriteria.type}
+          />
+          {Object.values(ProjectType).map((type) => (
+            <RadioButton
+              key={type}
+              label={type}
+              id={type}
+              value={type}
+              name="type"
+              onChange={handleTypeChange}
+              checked={searchCriteria.type === type}
+            />
+          ))}
+        </SearchContainer>
+        <SearchContainer>
+          <FieldsetLabel>Platform</FieldsetLabel>
+
+          {Object.values(Platform).map((platform) => (
+            <Checkbox
+              key={platform}
+              label={platform}
+              id={platform}
+              value={platform}
+              name="platform"
+              onChange={handlePlatformChange}
+              checked={searchCriteria.platforms?.has(platform)}
+            />
+          ))}
+        </SearchContainer>
+      </NonDisplayFilterOptions>
+      <ResetSearchContainer>
+        <Button onClick={handleResetSearch}>Reset Search</Button>
+      </ResetSearchContainer>
     </Container>
   )
 }
