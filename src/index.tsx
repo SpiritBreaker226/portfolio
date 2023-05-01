9cf6da50c3de1f4e6cb1086b491df51e74361c99
Jason Stathopulos <jstathopulos@gmail.com>
2023-04-30 20:31:34 -0700

    Add Sentry For Monitoring
    Add Sentry for monitoring, as there may be bugs in production that the
    developer must be aware of. Sentry will be able to find those bugs and
    present them on the interact page with more details on the bug.
    Furthermore, Sentry can also ensure the performance goes as fast as
    possible.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  environment: process.env.NODE_ENV,
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1.0,
+  // Session Replay
+  replaysSessionSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1.0,
+  replaysOnErrorSampleRate: 1.0,
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
