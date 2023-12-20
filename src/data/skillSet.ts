fa93110f7a63004f480be12eb2ab8765304734f5
Jason Stathopulos <jstathopulos@gmail.com>
2023-12-20 11:19:42 -0500

    Update Skill Set For 2023
    Update the skill set for 2023, as some computer languages have been used
    for over a decade now. While others' skills are rusty; however, they may
    still be used.

--- src/data/skillSet.ts
+++ src/data/skillSet.ts
@@ -2,21 +2,17 @@ import { Tag } from '../types'
 
 type SkillSet = { [k in Tag]: number }
 
-export const skillSet: SkillSet = {
+export const skillSet: Partial<SkillSet> = {
   'Ruby on Rails': 6.5,
   Ruby: 5.5,
   'C++': 5,
-  'C#': 3,
-  'Objective-C': 3,
-  PHP: 4,
   JavaScript: 8.5,
   ReactJS: 8,
-  Phonegap: 4,
   HTML: 7.5,
   CSS: 6,
-  SQL: 7,
-  'SQL Server': 7,
-  PostgreSQL: 6.5,
-  mySQL: 5.5,
+  SQL: 6,
+  'SQL Server': 6,
+  PostgreSQL: 6,
+  mySQL: 5,
   Git: 9,
 }
